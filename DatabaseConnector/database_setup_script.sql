b18283b3da64d89280a60d630a7c90bd5e222eef
Benbot16 <9423435+benbot16@users.noreply.github.com>
2024-02-08 16:06:35 -0800

    Updates database code to be more robust

--- DatabaseConnector/database_setup_script.sql
+++ DatabaseConnector/database_setup_script.sql
@@ -5,44 +5,45 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema stocktracker
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema stocktracker
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `stocktracker` DEFAULT CHARACTER SET utf8 ;
+CREATE SCHEMA IF NOT EXISTS `stocktracker` ;
 USE `stocktracker` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`tracked_tickers`
+-- Table `stocktracker`.`tracked_tickers`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `stocktracker`.`tracked_tickers` (
   `ticker` CHAR(5) NOT NULL,
-  PRIMARY KEY (`ticker`),
-  UNIQUE INDEX `ticker_UNIQUE` (`ticker` ASC) VISIBLE)
+  `tickerid` INT(10) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`tickerid`),
+  UNIQUE INDEX `ticker_UNIQUE` (`ticker` ASC) VISIBLE,
+  UNIQUE INDEX `tickerid_UNIQUE` (`tickerid` ASC) VISIBLE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`ticker_dataset`
+-- Table `stocktracker`.`ticker_dataset`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `stocktracker`.`ticker_dataset` (
-  `ticker` CHAR(5) NOT NULL,
-  `timestamp` DATE NOT NULL,
+  `tickerid` INT(10) NOT NULL,
+  `timestamp` DATETIME NOT NULL,
   `interval` INT NOT NULL,
-  `open` DECIMAL(2) UNSIGNED NOT NULL,
-  `high` DECIMAL(2) UNSIGNED NOT NULL,
-  `low` DECIMAL(2) UNSIGNED NOT NULL,
-  `close` DECIMAL(2) UNSIGNED NOT NULL,
-  PRIMARY KEY (`ticker`, `timestamp`, `interval`),
-  CONSTRAINT `ticker`
-    FOREIGN KEY (`ticker`)
-    REFERENCES `mydb`.`tracked_tickers` (`ticker`)
-    ON DELETE CASCADE
-    ON UPDATE RESTRICT)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = ascii;
+  `open` DECIMAL(10,2) UNSIGNED NOT NULL,
+  `high` DECIMAL(10,2) UNSIGNED NOT NULL,
+  `low` DECIMAL(10,2) UNSIGNED NOT NULL,
+  `close` DECIMAL(10,2) UNSIGNED NOT NULL,
+  PRIMARY KEY (`tickerid`, `timestamp`, `interval`),
+  CONSTRAINT `fk_ticker_dataset_1`
+    FOREIGN KEY (`tickerid`)
+    REFERENCES `stocktracker`.`tracked_tickers` (`tickerid`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
