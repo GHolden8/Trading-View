9d5b63a8329133fd36e4ba7d6c02eac35755837d
Noah Black <alaskanhighlander1@gmail.com>
2024-02-09 17:04:17 -0800

    added proprietary MySQL Connector class, DB_connect.json config file

--- DatabaseConnector/database_setup_script.sql
+++ DatabaseConnector/database_setup_script.sql
@@ -11,6 +11,10 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema stocktracker
 -- -----------------------------------------------------
+
+-- Added to nuke database so all tables are wiped to decrease likelyhood of persistent old tables
+DROP DATABASE `stocktracker` IF EXISTS;
+
 CREATE SCHEMA IF NOT EXISTS `stocktracker` ;
 USE `stocktracker` ;
 
