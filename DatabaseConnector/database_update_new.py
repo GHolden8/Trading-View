77f70d99eef818535d97f3d1e566bd9c1b459f27
Noah Black <alaskanhighlander1@gmail.com>
2024-02-09 18:34:46 -0800

    refactored data retrieval function to use db interface and av API interface

--- DatabaseConnector/database_update_new.py
+++ DatabaseConnector/database_update_new.py
@@ -28,9 +28,9 @@ config_file.close()
 
 def insert_ticker(ticker):
     insert = f"""
-    INSERT INTO tracked_tickers
-    `ticker` VALUES ("{ticker}")
-    ;
+        INSERT INTO tracked_tickers
+        `ticker` VALUES ("{ticker}")
+        ;
     """
     dbi.sql_execute(insert)
 
@@ -40,61 +40,59 @@ def insert_ticker(ticker):
 # Exceptions thrown: RequestsException
 def update_AV_data(func, symbol, interval):
 
-    data = get_series(func, symbol, interval)
+    api_data = get_series(func, symbol, interval)
 
     query = f"""
-    SELECT DISTINCT tickerid
-    FROM tracked_tickers 
-    WHERE ticker = '{symbol}'
-    ;
+        SELECT DISTINCT tickerid
+        FROM tracked_tickers 
+        WHERE ticker = '{symbol}'
+        ;
     """
     db_out = dbi.sql_select(query)
     if len(db_out) == 0:
         # ticker does NOT exist in tracked ticker table. Inserting.
         insert_ticker(symbol)
-
+        # reacquire ticker id
+        db_out = dbi.sql_select(query)
+        tickerid = db_out[0][0]
     else:
         # ticker exists in tracked ticker table, assign ticker_id
         tickerid = db_out[0][0]
 
+    for key in api_data:
+        if(key != "Meta Data"):
+            for date in api_data[key]:
+                open_ = round(float(api_data[key][date]["1. open"]), 2)
+                high = round(float(api_data[key][date]["2. high"]), 2)
+                low = round(float(api_data[key][date]["3. low"]), 2)
+                close = round(float(api_data[key][date]["4. close"]), 2)
 
+                # /!\ WARNING: QUERY DOES NOT ACCOUNT FOR INTERVAL /!\
+                insert = f"""
+                    INSERT INTO ticker_dataset
+                    VALUES (
+                        "{tickerid}",
+                        "{date}",
+                        {int(interval)},
+                        {open_},
+                        {high},
+                        {low},
+                        {close}
+                    )
+                    ON DUPLICATE KEY UPDATE
+                """
+                dbi.sql_execute(insert)
+
+        else:
+            print("API Failure. No Data Returned.")
+            print(api_data)
+            exit(1)
 
     # ======================================================================== 
 
-    # Get the ID from the symbol
-    mycursor.execute(f"SELECT tickerid FROM tracked_tickers WHERE ticker = '{symbol}'")
-    tickerid = mycursor.fetchall()
-    tickerid = tickerid[0][0]
-
-    # This could be a single query, just do a join on the ticker_dataset table.
-    if(not tickerid):
-        mycursor.execute(f"INSERT INTO tracked_tickers VALUES ('{symbol}')")
-        mycursor.execute(f"SELECT tickerid FROM tracked_tickers WHERE ticker = '{symbol}'")
-        tickerid = mycursor.fetchall()
-
-    # Update the database
-    for key in data:
-        if(key != "Meta Data"):
-            for date in data[key]:
-                open_ = round(float(data[key][date]["1. open"]), 2)
-                high = round(float(data[key][date]["2. high"]), 2)
-                low = round(float(data[key][date]["3. low"]), 2)
-                close = round(float(data[key][date]["4. close"]), 2)
-                #print(
-                #    f"""
-                #    INSERT INTO ticker_dataset
-                #    VALUES ("{symbol}","{date}",{interval},{open_},{high},{low},{close})
-                #    """)
-                mycursor.execute(
-                    f"""
-                    INSERT INTO ticker_dataset
-                    VALUES ("{tickerid}","{date}",{int(interval)},{open_},{high},{low},{close})
-                    ON DUPLICATE KEY UPDATE"""
-                )
-    mydb.commit()
 
 # Main function - FOr testing purposes
-if(__name__ == "__main__"
+if __name__ == "__main__":
     # Get the connection info
     coninfo = open("./config/db_info.cfg", 'r')
 
