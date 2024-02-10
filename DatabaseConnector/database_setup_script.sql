3911bfd0cca858289cbeff8a196218a85c6435a1
Benbot16 <9423435+benbot16@users.noreply.github.com>
2024-02-10 09:37:46 -0800

    Fixes an error in the DB setup script

--- DatabaseConnector/database_setup_script.sql
+++ DatabaseConnector/database_setup_script.sql
@@ -13,7 +13,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 
 -- Added to nuke database so all tables are wiped to decrease likelyhood of persistent old tables
-DROP DATABASE `stocktracker` IF EXISTS;
+DROP DATABASE IF EXISTS `stocktracker`;
 
 CREATE SCHEMA IF NOT EXISTS `stocktracker` ;
 USE `stocktracker` ;
