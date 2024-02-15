7aaf62caa74516e6a6bd0656c8b2a2ed1552b1c0
Garrett Holden <holden_170_@hotmail.com>
2024-02-15 15:47:59 -0800

    function beginning

--- REACTPlayground/stockbase/src/stockGraph.js
+++ REACTPlayground/stockbase/src/stockGraph.js
@@ -1,6 +1,20 @@
 // StockExaminer.js
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Line } from 'react-chartjs-2';
 
 //creating candle graph
+const candleStickGraph = () => {
+const data = {
+    datasets: [
+      {
+        label: 'Candlestick Chart',
+        data: [
+          //{ t: new Date(2024, 2, 15), o: 100, h: 100, l: 100, c: 100 }, take from database in some similar form
 
+        ],
+        borderColor: 'green',
+        backgroundColor: 'black',
+      },
+    ],
+  };
+}
