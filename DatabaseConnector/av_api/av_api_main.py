f82c3dbace0977b4303126842296590091bd924f
Benbot16 <9423435+benbot16@users.noreply.github.com>
2024-02-27 14:26:55 -0800

    Fixing lint errors in AV API

--- DatabaseConnector/av_api/av_api_main.py
+++ DatabaseConnector/av_api/av_api_main.py
@@ -1,15 +1,19 @@
-import requests
+''' This module is the main module for the alpha vantage API.
+    It contains the main functions for interfacing with the API. '''
+
+import sys
 import json
 import os
 
 # from DatabaseConnector.av_api.alpha_vantage.timeseries import TimeSeries
 from alpha_vantage import timeseries
-from DatabaseConnector.av_api.av_api_exceptions import InvalidFunctionException, InvalidIntervalException
+from DatabaseConnector.av_api.av_api_exceptions import\
+     InvalidFunctionException, InvalidIntervalException
 
 CONFIGS = None
 current_dir = os.path.dirname(__file__)
 config_path = os.path.join(current_dir, "api_config.json")
-config_file = open(config_path)
+config_file = open(config_path, "r")
 CONFIGS = json.load(config_file)
 config_file.close()
 
@@ -19,14 +23,16 @@ API_KEY = CONFIGS['av_api_keys'][0]
 
 
 
-def get_series(function, symbol, interval, api_key, month = None, full=False):
-    '''This function will return a dictionary of time series data for the specified symbol and time interval.'''
+def get_series(function, symbol, interval, api_key, full=False):
+    '''This function will return a dictionary of time series data
+        for the specified symbol and time interval.'''
     ts = timeseries.TimeSeries(key=api_key, output_format='json')
 
     try:
         # Get time series data based on function, symbol, and interval
         if function == 'intraday':
-            data, _ = ts.get_intraday(symbol=symbol, interval=interval, month = month, outputsize='full' if full else 'compact')
+            data, _ = ts.get_intraday(symbol=symbol, interval=interval,\
+                                    outputsize=('full' if full else 'compact'))
         elif function == 'daily':
             data, _ = ts.get_daily(symbol=symbol, outputsize='full' if full else 'compact')
         elif function == 'weekly':
@@ -35,27 +41,27 @@ def get_series(function, symbol, interval, api_key, month = None, full=False):
             data, _ = ts.get_monthly(symbol=symbol, outputsize='full' if full else 'compact')
         else:
             raise ValueError("Invalid function provided: " + function)
-        
         return data
 
     except ValueError as e:
         print(e)
-        exit(1)
+        sys.exit(1)
 
 def get_time_series(function, symbol, interval, start_date, end_date, month, api_key):
-    '''This function will return a dictionary of time series data for the specified symbol and time period. 
+    '''This function will return a dictionary of time series data for the 
+        specified symbol and time period. 
         The time series data will be filtered based on the specified time interval.'''
 
     try:
         # Get time series data within the specified time period
         data = get_series(function, symbol, interval, api_key, month, True)
-        
+
         # Filter data within the specified time period
         filtered_data = {}
-        for date, values in data.items():
+        for date, values in data:
             if start_date <= date <= end_date:
                 filtered_data[date] = values
-        
+
         return filtered_data
 
     except Exception as e:
@@ -63,7 +69,7 @@ def get_time_series(function, symbol, interval, start_date, end_date, month, api
         exit(1)
 
 def validate_function(function):
-    "Alpha vantage API Function validation function"
+    '''Alpha vantage API Function validation function'''
     valid_functions = [
         "TIME_SERIES_INTRADAY",
         "TIME_SERIES_DAILY",
@@ -71,7 +77,7 @@ def validate_function(function):
     ]
     if function not in valid_functions:
         raise InvalidFunctionException(f"{function} is not a valid function. Check Docs.")
-    
+
 def validate_interval(interval):
     intervals = [
         '1min',
@@ -85,9 +91,10 @@ def validate_interval(interval):
     ]
     if interval not in intervals:
         raise InvalidIntervalException(f"{interval} is not a valid time interval.")
-    
+
 # Testing stuff
 if __name__ == "__main__":
-    response = get_time_series('TIME_SERIES_DAILY', 'AAPL', None, '2023-01-03', '2023-01-05', '2023-01', API_KEY)
+    response = get_time_series('TIME_SERIES_DAILY', 'AAPL', None, '2023-01-03',\
+                                '2023-01-05', '2023-01', API_KEY)
     with open("av_test_data.json", "w") as file:
         json.dump(response, file, ensure_ascii=False, indent=4)
--- DatabaseConnector/av_api/av_database_update.py
+++ DatabaseConnector/av_api/av_database_update.py
@@ -6,13 +6,12 @@ import os
 
 from DatabaseConnector.av_api.av_api_main import get_series
 from DatabaseConnector import mysql_connect
-from DatabaseConnector.database_utils import *
+from DatabaseConnector.database_utils import insert_candle
 
 CONFIGS = None
 config_path = os.path.join(os.getcwd(), "DatabaseConnector/av_api/api_config.json")
-config_file = open(config_path)
-CONFIGS = json.load(config_file)
-config_file.close()
+with open(config_path, "r") as config_file:
+    CONFIGS = json.load(config_file)
 
 ENDPOINT = CONFIGS['api_endpoint']
 API_KEY = CONFIGS['av_api_keys'][0]
@@ -21,13 +20,11 @@ API_KEY = CONFIGS['av_api_keys'][0]
 DB_CONFIGS = None
 current_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 config_path = os.path.join(current_dir, "db_config.json")
-config_file = open(config_path)
+with open(config_path, "r") as config_file:
+    DB_CONFIGS = json.load(config_file)
 
 # database interface object
 dbi = mysql_connect.MySQLConnect(config_path)
-
-DB_CONFIGS = json.load(config_file)
-config_file.close()
 
 def av_database_update(interval='TIME_SERIES_DAILY', fullness=False, target_stock=None):
     '''This function updates the database with the latest stock data from the Alpha Vantage API.'''
@@ -47,7 +44,7 @@ def av_database_update(interval='TIME_SERIES_DAILY', fullness=False, target_stoc
     # Get the latest stock data for each ticker
     for ticker in tickers:
         # Get the latest stock data for the ticker
-        if(isinstance(ticker, list)):
+        if isinstance(ticker, list):
             symbol = ticker[0]
         else:
             symbol = ticker
@@ -55,4 +52,5 @@ def av_database_update(interval='TIME_SERIES_DAILY', fullness=False, target_stoc
 
         # Insert the latest stock data into the database
         for date, values in data.items():
-            insert_candle(symbol, date, interval, values['1. open'], values['2. high'], values['3. low'], values['4. close'])
+            insert_candle(symbol, date, interval, values['1. open'], \
+                          values['2. high'], values['3. low'], values['4. close'])
