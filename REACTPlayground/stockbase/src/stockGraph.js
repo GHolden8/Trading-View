6327a64e1dc80b8e699b62ab7eade75799fe9870
GHolden8 <117676816+GHolden8@users.noreply.github.com>
2024-02-27 15:11:44 -0800

    prepared hopefully for json file input
    I currently set this up I believe for a single company at a time

--- REACTPlayground/stockbase/src/stockGraph.js
+++ REACTPlayground/stockbase/src/stockGraph.js
@@ -1,20 +1,41 @@
-// Candle Stick Graph
+
+// Takes Json input to create candle graph
+import React, { useEffect, useState } from 'react';
+// candle stick graph function and import
+//may need npm install for chart2 to run
 import React from 'react';
 import { Line } from 'react-chartjs-2';
 
-//creating candle graph
-const candleStickGraph = () => {
-const data = {
-    datasets: [
-      {
-        label: 'Candlestick Chart',
-        data: [
-          //{ t: new Date(2024, 2, 15), o: 100, h: 100, l: 100, c: 100 }, take from database in some similar form
+const CandlestickChart = () => {
+  const [chartData, setChartData] = useState({});
 
-        ],
-        borderColor: 'green',
-        backgroundColor: 'black',
-      },
-    ],
+//should get data from file
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        //const response = await fetch('touchGrass.file');   need file
+        const jsonData = await response.json();
+        const formattedData = formatData(jsonData);
+        setChartData(formattedData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const formatData = (data) => {
+    // I think this is current format???
+    // { datasets: [{ data: [{ t: 1, h: 15, l: 5, c: 12 }] }] } time, high, low, close?
   };
-}
+
+  return (
+    <div>
+      //<h2>*Name of current company*</h2> heading if needed can pull company_name if in database
+      <Line data={chartData} />
+    </div>
+  );
+};
+
+export default CandlestickChart;
