b596a2fc26ef0b0be9f9e3721513fc3e572ac87b
Benbot16 <9423435+benbot16@users.noreply.github.com>
2024-02-27 16:30:01 -0800

    Fixes

--- DatabaseConnector/av_api/av_api_main.py
+++ DatabaseConnector/av_api/av_api_main.py
@@ -94,6 +94,6 @@ def validate_interval(interval):
 # Testing stuff
 if __name__ == "__main__":
     response = get_time_series('TIME_SERIES_DAILY', 'AAPL', None, '2023-01-03',\
-                                '2023-01-05', '2023-01', API_KEY)
+                                '2023-01-05', API_KEY)
     with open("av_test_data.json", "w") as file:
         json.dump(response, file, ensure_ascii=False, indent=4)
