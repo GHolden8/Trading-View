1cd6060f111a4c6c46a6afe9c1d60f7bb4eccb3f
Benbot16 <9423435+benbot16@users.noreply.github.com>
2024-02-28 16:06:29 -0800

    Fixes a test'

--- tests/av_sql_test.py
+++ tests/av_sql_test.py
@@ -4,13 +4,7 @@ import unittest
 
 from DatabaseConnector.av_api.av_api_main import get_time_series
 
-# Grab our API key from secrets
-try:
-    # OS environ not conducive to cross-platform dev, lets change this to a config. Private Repo
-    API_KEY = os.environ['TEST_API_KEY_AV']
-
-except:
-    API_KEY = "demo"
+API_KEY = "ZFH8GYRZVTGCPCIV"
 
 class av_sql_test(unittest.TestCase):
     '''Tests the get_time_series function against a given JSON file.'''
