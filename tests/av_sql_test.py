35d05366ce7f6edab3a8091a4531d526b6d0f593
Benbot16 <9423435+benbot16@users.noreply.github.com>
2024-02-28 16:19:23 -0800

    Fix the second

--- tests/av_sql_test.py
+++ tests/av_sql_test.py
@@ -7,7 +7,7 @@ from DatabaseConnector.av_api.av_api_main import get_time_series
 class av_sql_test(unittest.TestCase):
     '''Tests the get_time_series function against a given JSON file.'''
     def test_sql(self):
-        response = get_time_series('TIME_SERIES_DAILY', 'AAPL', \
+        response = get_time_series('daily', 'AAPL', \
                     None, '2023-01-03', '2023-01-05', "ZFH8GYRZVTGCPCIV")
         test_data = json.load(open('av_test_data.json', 'r'))
         self.assertEqual(test_data, response)
