a2a20a3bce945a8355261bf0d4f5dc7808952a2b
Benbot16 <9423435+benbot16@users.noreply.github.com>
2024-03-04 22:35:03 -0800

    Adds a YF update script

---
+++ autoupdate.py
@@ -0,0 +1,43 @@
+'''
+Automatic daily database update script.
+Updates the database each day.
+'''
+from time import sleep, time
+from DatabaseConnector.database_utils import bulk_download
+from DatabaseConnector.yahoo_finance.yahooFinance import modtime
+
+
+STOCKS = [
+    'AAPL'
+    'MSFT',
+    'NVDA',
+    'GOOGL',
+    'META',
+    'BRK.B',
+    'LLY',
+    'TSLA',
+    'AVGO',
+    'V',
+    'JPM',
+    'UNH',
+    'MA',
+    'HD',
+    'AMZN',
+    'XOM'
+]
+
+INTERVAL_LIST = [
+    'daily'
+]
+
+if __name__ == "__main__":
+    epoch_time = int(time())
+    lastmod = modtime(epoch_time, "weekly")
+
+    while True:
+        print("Initiating daily stock update...")
+        for interval in INTERVAL_LIST:
+            bulk_download(STOCKS, lastmod, time(), interval)
+
+        lastmod = int(time()) + 86400 # Last day plus one day
+        sleep(lastmod - int(time()))
