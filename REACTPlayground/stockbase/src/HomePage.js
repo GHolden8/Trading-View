b47756049ba83112c441cfd66488a883b13e7481
Jake L <ienderjakemc@gmail.com>
2024-03-05 17:02:00 -0800

    message

--- REACTPlayground/stockbase/src/HomePage.js
+++ REACTPlayground/stockbase/src/HomePage.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
+var tempData = "Test Data";
 /*
 JSON DATA ORDER:
 0 - Date/time
@@ -13,29 +14,30 @@ JSON DATA ORDER:
 
 */
 
-
-async function getStockHistory() {
-  try {
-    let response = await fetch('http://127.0.0.1:8080/GOOGL/daily');
-    let responseJson = await response.json();
-    return responseJson;
-   } catch(error) {
-    console.error(error);
-  }
-}
-
-
 function HomePage() {
   const navigate = useNavigate();
-  const googleData = getStockHistory();
   //const googleRecent = ;
-  
+  var opts = {
+    headers: {
+      'mode':'no-cors'
+    }
+  }
+    fetch('http://127.0.0.1:8080/GOOGL/daily', opts)
+        .then(response => response.json())
+        .then(data => {
+            // Handle the JSON data here
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Error fetching JSON:', error);
+        });
+        
   return (
     <div>
       <h1>Welcome to the Home Page!</h1>
       <button onClick={() => navigate('/portfolio')}>Go to Portfolio Page</button>
       <button onClick={() => navigate('/stockexaminer')}>Go to Stock Page</button>
-      <h1>The most recent Google share price is: {googleData.data}</h1>
+      <h1>The most recent Google share price is: {tempData}</h1>
     </div>
   );
 }
