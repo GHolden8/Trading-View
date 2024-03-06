3dd60fe32ced2d7f921014423d6a6e63eae2b281
GHolden8 <holden_170_@hotmail.com>
2024-03-05 16:53:31 -0800

    update

--- REACTPlayground/stockbase/src/StockExaminer.js
+++ REACTPlayground/stockbase/src/StockExaminer.js
@@ -1,8 +1,8 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-<
+
 <header>
-Header set Access-Control-Allow-Origin: 'https://example.com'
+Header set Access-Control-Allow-Origin: 'http://127.0.0.1:8080/GOOGL/daily'
 
 </header>
 function StockExaminer() {
