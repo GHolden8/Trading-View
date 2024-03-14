884b0b752af7d930d19def530943f037240db97b
Jake L <ienderjakemc@gmail.com>
2024-03-14 10:03:35 -0700

    Created CSS files and added stock picking row

---
+++ REACTPlayground/stockbase/src/tableStyle.jsx
@@ -0,0 +1,40 @@
+const tableOneStyles = {
+    headCells: {
+      style: {
+        fontSize: '20px',
+        fontWeight: 'bold',
+        color: 'white',
+        paddingLeft: '0 8px',
+        justifyContent: 'center',
+        backgroundColor: '#333'
+      },
+    },
+    cells: {
+        style: {
+            fontSize: '16px',
+            backgroundColor: '#c0c0c0',
+            color: 'green'
+        },
+    },
+  }
+
+  const tableTwoStyles = {
+    headCells: {
+      style: {
+        fontSize: '20px',
+        fontWeight: 'bold',
+        color: 'white',
+        paddingLeft: '0 8px',
+        justifyContent: 'center',
+        backgroundColor: '#333'
+      },
+    },
+    cells: {
+        style: {
+            fontSize: '16px',
+            backgroundColor: '#d0d0d0',
+            color: 'red',
+        },
+    }
+  }
+  export { tableOneStyles, tableTwoStyles };
